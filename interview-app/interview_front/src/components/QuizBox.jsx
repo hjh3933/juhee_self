63a3ec4e2587f768012fca0886af0c09530e223f
hjh3933 <hkh3933@naver.com>
2026-03-12 23:09:19 +0900

    0312

--- interview-app/interview_front/src/components/QuizBox.jsx
+++ interview-app/interview_front/src/components/QuizBox.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
+import "../styles/quizBox.scss";
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowBack } from "react-icons/io";
 
-export function QuizBox({ quizData }) {
-  // category, quiz, answer
+export function QuizBox({ quizData, prevQuiz, nextQuiz }) {
+  // category, quiz, answer, nowPage, totalPage
   // 전체 문제 수와 현재 문제 위치 받기
   const [toggle, setToggle] = useState(true);
   return (
@@ -13,20 +16,36 @@ export function QuizBox({ quizData }) {
         </span>
       </div>
       <div className="qBoxContent">
-        <div className="arrow arrL">왼쪽 화살표</div>
+        <div
+          className="arrow"
+          onClick={() => {
+            setToggle(true);
+            prevQuiz();
+          }}
+        >
+          <IoIosArrowBack className="arrIcon" />
+        </div>
         <div className="quizContainer">
           <div className="textBox">
             {toggle ? (
-              <span className="quiz">{quizData.quiz}</span>
+              <span className="quiz">Q. {quizData.quiz}</span>
             ) : (
-              <span className="answer">{quizData.answer}</span>
+              <span className="answer">A. {quizData.answer}</span>
             )}
           </div>
           <div className="toggleBtn">
             <button onClick={() => setToggle(!toggle)}>{toggle ? "답변확인" : "문제확인"}</button>
           </div>
         </div>
-        <div className="arrow arrR">오른쪽 화살표</div>
+        <div
+          className="arrow"
+          onClick={() => {
+            setToggle(true);
+            nextQuiz();
+          }}
+        >
+          <IoIosArrowForward className="arrIcon" />
+        </div>
       </div>
     </div>
   );
--- interview-app/interview_front/src/pages/QuizPage.jsx
+++ interview-app/interview_front/src/pages/QuizPage.jsx
@@ -1,24 +1,35 @@
 import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
 import { QuizBox } from "../components/QuizBox";
+import { quizList } from "../data/quizData";
+import { useState } from "react";
 
 export function QuizPage() {
   // url로 받음
   const { category } = useParams();
   // 임시 퀴즈 데이터
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentQuiz = quizList[currentIndex];
+
   const quizData = {
-    category: category,
-    quiz: "1분 자기소개",
-    answer:
-      "안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 ",
-    totalPage: 13,
-    nowPage: 3,
+    category,
+    quiz: currentQuiz.quiz,
+    answer: currentQuiz.answer,
+    nowPage: currentIndex + 1,
+    totalPage: quizList.length,
+  };
+  // 이동버튼
+  const prevQuiz = () => {
+    setCurrentIndex((prev) => (prev > 0 ? prev - 1 : prev));
+  };
+  const nextQuiz = () => {
+    setCurrentIndex((prev) => (prev < quizList.length - 1 ? prev + 1 : prev));
   };
   return (
     <div className="container">
       <Header />
       <div className="pageContainer">
-        <QuizBox quizData={quizData} />
+        <QuizBox quizData={quizData} prevQuiz={prevQuiz} nextQuiz={nextQuiz} />
       </div>
     </div>
   );
